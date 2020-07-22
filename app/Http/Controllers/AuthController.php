cd3c9da52636df7ce4aaae84126204ee8c703215
bartsv <b.bartsv@gmail.com>
2020-07-22 12:00:38 +0200

    login e registrazione ok

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -26,7 +26,7 @@ class AuthController extends Controller {
     public function login(Request $request){
         $validator = Validator::make($request->all(), [
             'email' => 'required|email',
-            'password' => 'required|string|min:8',
+            'password' => 'required|string|min:6',
         ]);
 
         if ($validator->fails()) {
@@ -37,7 +37,7 @@ class AuthController extends Controller {
             return response()->json(['error' => 'Unauthorized'], 401);
         }
 
-        return $this->createNewToken($token);
+        return response()->json(['mail'=>$request->input('email'),'token'=>$this->createNewToken($token)],200) ;
     }
 
     /**
@@ -49,6 +49,7 @@ class AuthController extends Controller {
         $validator = Validator::make($request->all(), [
             'name' => 'required|string|between:2,100',
             'email' => 'required|string|email|max:100|unique:users',
+            'password' => 'required|string|confirmed|min:6',
         ]);
 
         if($validator->fails()){
--- public/js/prova.js
+++ public/js/prova.js
@@ -1,30 +1,36 @@
 function Login(url) {
-        $('log').click(function () {
-            var csrf_token = $('meta[name="csrf-token"]').attr('content');
+    $(document).ready(function() {
+        $('#log').click(function () {
             var mail = $("#defaultForm-email").val().trim();
             var psw = $("#defaultForm-pass").val();
             $.ajax({
                 url: url,
                 type: 'POST',
                 data: {
-                    name: name,
                     email: mail,
                     password: psw
                 },
+                dataType: 'json',
+                headers: {
+                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                },
                 success: function (data) {
-                    if ($.isEmptyObject(data.error)) {
-                        alert(data.success);
-                    } else {
-                        alert(data.error);
-                    }
+                    console.log(data);
+                    var mail=data.mail;
+                    var token=data.token;
+                    localStorage.setItem("mail", mail);
+                    localStorage.setItem("token", token);
+                },
+                error:function (data) {
+                console.log(data)
                 }
             });
         });
+    });
 }
 function Register(url) {
     $(document).ready(function() {
         $("#reg").click(function(){
-            var csrf_token = $('meta[name="csrf-token"]').attr('content');
             var name = $("#RdefaultForm-name").val().trim();
             var mail = $("#RdefaultForm-email").val().trim();
             var psw = $("#RdefaultForm-pass").val();
@@ -35,10 +41,15 @@ function Register(url) {
                 $.ajax({
                     url: url,
                     type: 'POST',
+                    dataType: 'json',
+                    headers: {
+                        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                    },
                     data: {
                         name: name,
                         email: mail,
-                        password: psw
+                        password: psw,
+                        password_confirmation:cpsw
                     },
                     success: function (data) {
                         if ($.isEmptyObject(data.error)) {
